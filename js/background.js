fe57aec0c4e75d8917318e1b1fd4c84994cdecbe
Yun, Hong In <akenfn513@gmail.com>
2022-10-27 11:01:34 +0900

    feat: background image

--- js/background.js
+++ js/background.js
@@ -1,9 +1,10 @@
-const images = ["0.jpeg", "1.jpeg", "2.jpeg"];
+const images = [
+  "https://images.unsplash.com/photo-1666809489316-76eb1f11cb4f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2532&q=80",
+  "https://images.unsplash.com/photo-1542370285-b8eb8317691c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1652&q=80",
+  "https://images.unsplash.com/photo-1542332213-9b5a5a3fad35?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80",
+];
 
 let i = Math.floor(Math.random() * images.length);
 const chosenImage = images[i];
 
-const bgImg = document.createElement("img");
-bgImg.src = `img/${chosenImage}`;
-
-document.body.appendChild(bgImg);
+document.body.style.backgroundImage = `url(${chosenImage})`;
